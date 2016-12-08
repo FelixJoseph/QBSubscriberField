f05137b7cdbea0a03caedfc63891f7d7331c35a8
Felix <felix@qburst.com>
2016-12-08 11:38:45 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/QBSubscriberField/QBSubscriberField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QBSubscriberField : NSObject
+@end
+@implementation PodsDummy_QBSubscriberField
+@end
---
+++ Example/Pods/Target Support Files/QBSubscriberField/QBSubscriberField-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double QBSubscriberFieldVersionNumber;
+FOUNDATION_EXPORT const unsigned char QBSubscriberFieldVersionString[];
+
